bbe0a6da063f71b996ae0289a9c0c8bbfe83016d
Kush Pamnani <kushpamnanii@gmail.com>
2024-07-31 18:48:26 -0400

    model, add, remove functions added

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
-import { Box, Typography } from "@mui/material";
-import { query, getDocs, collection } from "firebase/firestore";
+import { Box, stepClasses, Typography } from "@mui/material";
+import { query, getDocs, collection, getDoc, setDoc } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
@@ -21,12 +21,46 @@ export default function Home() {
       });
     });
     setInventory(inventoryList);
-    console.log(inventoryList)
   };
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore,'inventory'), item)
+    const docSnap = await getDoc (docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+        await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else {
+      await setDoc (docRef, {quantity: 1})
+    }
+
+    await updateInventory()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore,'inventory'), item)
+    const docSnap = await getDoc (docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if(quantity === 1){
+        await delete(docRef)
+      }
+      else {
+        await setDoc(docRef, {quantity: quantity - 1})
+      }
+    }
+
+    await updateInventory()
+  }
 
   useEffect(() => {
     updateInventory();
   }, []);
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setClose(false) //models defined
 
   return (
     <Box>
