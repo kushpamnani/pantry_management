457c06b3c5281e882d6af85d58ce852cd1d9fe39
Kush Pamnani <kushpamnanii@gmail.com>
2024-07-31 18:21:10 -0400

    adding initial files

---
+++ app/page.js
@@ -0,0 +1,43 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { firestore } from "@/firebase";
+import { Box, Typography } from "@mui/material";
+import { query, getDocs, collection } from "firebase/firestore";
+
+export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState("");
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  return (
+    <Box>
+      <Typography variant="h1">Inventory Management </Typography>
+      {inventory.forEach((item) => {
+        return (
+          <>
+            {item.name}
+            {item.count}
+          </>
+        );
+      })}
+    </Box>
+  );
+}
---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADnxR0U6N6o-PfRXM48ifWR8B7jBz4MeA",
+  authDomain: "inventory-management-781e6.firebaseapp.com",
+  projectId: "inventory-management-781e6",
+  storageBucket: "inventory-management-781e6.appspot.com",
+  messagingSenderId: "940090925396",
+  appId: "1:940090925396:web:85e11e7ebc708deb47a8c1",
+  measurementId: "G-F62V76E5SX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export {firestore}
